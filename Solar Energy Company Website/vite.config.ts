7cdcb90a35e04b1db758e7d8a4797dfa1beac661
Agostino <agostinoscholes572@gmail.com>
2026-04-16 19:21:21 +0800

    Complete

--- Solar Energy Company Website/vite.config.ts
+++ Solar Energy Company Website/vite.config.ts
@@ -30,9 +30,6 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  build: {
-    outDir: 'build',
-  },
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
